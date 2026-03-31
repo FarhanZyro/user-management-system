ec27dea237a6118ec233e240423469668068b323
Shahriar <mdfshahriar0@gmail.com>
2026-03-31 23:18:46 +0300

    Delete user-management directory

--- user-management/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "user-management");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-session_start();
-?>
